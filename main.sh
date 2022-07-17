07a53b74b45ca49c8bd499a43b69cd43ea50a9c4
Sayafx <2413377054@qq.com>
2022-07-17 09:16:14 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 wget https://github.com/cloudreve/Cloudreve/releases/download/3.5.3/cloudreve_3.5.3_linux_amd64.tar.gz
-tar -zxvf /cloudreve_3.5.3_linux_amd64.tar.gz
+tar -zxvf cloudreve_3.5.3_linux_amd64.tar.gz
 # 赋予执行权限
 chmod +x ./cloudreve
 
