bead3412e4fec83e37ef18fd9a7e91971b016208
Sayafx <2413377054@qq.com>
2022-07-17 09:14:08 +0800

    upload

---
+++ main.sh
@@ -0,0 +1,7 @@
+wget https://github.com/cloudreve/Cloudreve/releases/download/3.5.3/cloudreve_3.5.3_linux_amd64.tar.gz
+tar -zxvf /cloudreve_3.5.3_linux_amd64.tar.gz
+# 赋予执行权限
+chmod +x ./cloudreve
+
+# 启动 Cloudreve
+./cloudreve
